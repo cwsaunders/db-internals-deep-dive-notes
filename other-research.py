54e2a46c80477d835e9019a8e736e1708cc2e54b
cwsaunders <csaunders5@liberty.edu>
2021-04-08 16:03:43 -0400

    graph dbs

--- other-research.py
+++ other-research.py
@@ -4,7 +4,9 @@ Document Databases: (This includes MongoDB and Kassandra)
 https://www.youtube.com/watch?v=Nh6Y7DgZDrg
 
 
-
+Graph Databases:
+https://www.youtube.com/watch?v=dGHSKpx4Xjs
+https://www.youtube.com/watch?v=REVkXVxvMQE
 
 
 
